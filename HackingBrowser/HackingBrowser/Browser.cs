87ab2b352587c7deff838ebc8b1187e5f06fdc2c
NextGenerationHackers <48824185+NextGenerationHackers@users.noreply.github.com>
2021-01-23 11:17:55 -0600

    Initially Loaded Project Onto Github
    V1 of A Browser For ShadowGhosts & GodHacks.

---
+++ HackingBrowser/HackingBrowser/Browser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HackingBrowser
+{
+    public partial class Browser : Form
+    {
+        public Browser()
+        {
+            InitializeComponent();
+            //Upgrade the default web browser
+            var appName = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe";
+            using (var Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true))
+                Key.SetValue(appName, 99999, RegistryValueKind.DWord);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Browser_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
